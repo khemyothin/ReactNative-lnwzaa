1aceb467b34fa9f9a6ac37f209512a269d673b5a
khemyothin <teww2024@gmail.com>
2026-01-31 14:26:46 +0700

    1

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { Link } from "expo-router";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  return (
   <ScrollView style={{ flex: 1}}>
     <View style ={styles.container}>
 
@@ -12,4 +14,19 @@ export default function Index() {
     </View>
 
   </ScrollView>
+  )
 }
+const styles = StyleSheet.create({
+  container: {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: 15
+  },
+  link: {
+
+  },
+  linkText:{
+
+  }
+})
