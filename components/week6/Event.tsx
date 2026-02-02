117ae3c6368913e3c2ff37c59b4653faf0e48d86
khemyothin <teww2024@gmail.com>
2026-02-02 22:05:20 +0700

    Update Event.tsx

--- components/week6/Event.tsx
+++ components/week6/Event.tsx
@@ -28,7 +28,7 @@ export default function TourFlatList(props:any) {
     
     return (
         <View style={props.style}>
-            <Text style={{ fontSize: 20 }}>Up Comimg Evrts</Text>
+            <Text style={{ fontSize: 20 }}>Up Comimg Evets</Text>
             <Text style={{ color: "grey" }}>What's the Worst That Could Happend</Text>
             <FlatList
                 horizontal={true}
